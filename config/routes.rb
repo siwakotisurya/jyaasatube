ae927368283799aeaaacc7454fd7a4fec5a66b40
siwakotisurya <siwakotisurya@outlook.com>
2015-10-18 13:22:35 +0545

    bootstrap js set as require in app/assets/javascripts/application.js

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'jyaasa_tubes/index'
+  root 'jyaasa_tubes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
