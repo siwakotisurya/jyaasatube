aa47a7f846f80caf673d6fc6c032db9a2d001f3b
siwakotisurya <siwakotisurya@outlook.com>
2015-10-18 16:18:56 +0545

    function form_error creaate in channels helper and display form error in chanels view new page

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,2 +1,13 @@
 module ChannelsHelper
+
+	def form_error
+		if @channel.errors.any?
+			@channel.errors.full_messages.each do |s|
+				'<div class="alert alert-danger">
+	  			<strong>#{s}</strong>
+				</div>'.html_safe 
+			end
+			
+		end
+	end
 end
