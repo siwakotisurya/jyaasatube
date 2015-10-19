98fc535483b5209f27ebfab93d822704b9040c3d
siwakotisurya <siwakotisurya@outlook.com>
2015-10-19 10:01:50 +0545

    subscription odel created and migration run

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018163340) do
+ActiveRecord::Schema.define(version: 20151019041636) do
 
   create_table "channels", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151018163340) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "status",     limit: 255
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "channel_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,14 +49,15 @@ ActiveRecord::Schema.define(version: 20151018163340) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "videos", force: :cascade do |t|
-    t.string   "video",           limit: 255
-    t.string   "video_file_name", limit: 255
-    t.text     "video_meta",      limit: 65535
-    t.string   "video_file_size", limit: 255
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.integer  "channel_name",    limit: 4
-    t.integer  "channel_id",      limit: 4
+    t.string   "video",              limit: 255
+    t.string   "video_file_name",    limit: 255
+    t.text     "video_meta",         limit: 65535
+    t.string   "video_file_size",    limit: 255
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "channel_name",       limit: 4
+    t.integer  "channel_id",         limit: 4
+    t.string   "video_content_type", limit: 255
   end
 
 end
